18e0e9b47e07f5dac70a06183e6d28df5d2cbf65
callumbirks <callumbirks@protonmail.com>
2022-03-18 18:07:38 +0000

    Initial Commit

---
+++ Space Merchant/Space_MerchantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_MerchantApp.swift
+//  Space Merchant
+//
+//  Created by Callum Birks on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Space_MerchantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
